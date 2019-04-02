5dcc62104aef002763e414d232b91440a0a79991
Hai Tran <hai.tran@rxpservices.com>
2019-04-03 08:42:11 +1100

    ARM template for web app

--- deploy/ARM/deploy.ps1
+++ deploy/ARM/deploy.ps1
@@ -75,7 +75,7 @@ Write-Host "Selecting subscription '$subscriptionId'";
 Select-AzureRmSubscription -SubscriptionID $subscriptionId;
 
 # Register RPs
-$resourceProviders = @("microsoft.web");
+$resourceProviders = @("microsoft.web","microsoft.insights");
 if($resourceProviders.length) {
     Write-Host "Registering resource providers"
     foreach($resourceProvider in $resourceProviders) {
